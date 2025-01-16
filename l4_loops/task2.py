dcbfebce8e190c33f493cda140099442d7c12386
Vladislav20006 <sergio20002306@gmail.com>
2025-01-16 17:41:28 +0200

    Completed L4

---
+++ l4_loops/task2.py
@@ -0,0 +1,23 @@
+def odd_str(n: int) -> str:
+    s = ""
+    
+    # Use for loop to run from 0 to n (included)
+    # and form a string `s`.
+    # String `s` will contain only odd numbers from interval [0; n].
+    #
+    # For example,
+    # (0, 1, 2, 3, 4) -> "13"
+    # (0, 1, 2, 3, 4, 5, 6) -> "135" 
+
+    for i in range(n + 1):
+        if i % 2 != 0:  # Check if the number is odd
+            s += str(i)  # Append the odd number to the string
+    print(s)
+    return s
+
+
+# Do not change the below's code
+if __name__ == "__main__":
+    assert odd_str(4) == "13"
+    assert odd_str(6) == "135"
+    assert odd_str(8) == "1357"
---
+++ l4_loops/task5.py
@@ -0,0 +1,17 @@
+# Use `for` loop to check if character `c` is present in
+# string `s`.
+#
+# Return `True` is character is present. Return `False` otherwise
+def has_char(s: str, c: str) -> bool:
+    for char in s:
+        if char == c:
+            print(True)
+            return True
+        print(False)
+    return False
+
+
+# Do not change the below's code
+if __name__ == "__main__":
+    assert has_char("lfhyt", "f") == True
+    assert has_char("abbaabba", "c") == False
