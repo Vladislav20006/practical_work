5e0ebf9c3a020b029bcb9de40f3950072e6f1233
Vladislav20006 <sergio20002306@gmail.com>
2025-01-16 12:49:37 +0200

    Completed L1

---
+++ l1_variables_and_math/task11.py
@@ -0,0 +1,10 @@
+if __name__ == "__main__":
+    # Do not change the line below
+    s = "Hello World"
+
+    # Modify variable c using string s to make this script work without errors
+    c = s = "HloWrd"
+    print(c)
+
+    # Do not change the line below
+    assert c == "HloWrd"
