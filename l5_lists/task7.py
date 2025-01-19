e96e06d29fad55fdf19aa10b6ec03b1ddc0e9119
Vladislav20006 <sergio20002306@gmail.com>
2025-01-19 18:10:44 +0200

    Completed L5

---
+++ l5_lists/task7.py
@@ -0,0 +1,13 @@
+def proxy_pop(l: list[int]) -> int:
+    print(l)
+    return l.pop(-1)
+
+
+# Do not change the below's code
+if __name__ == "__main__":
+    l = [3, 6, 1, 2]
+
+    assert proxy_pop(l) == 2
+    assert proxy_pop(l) == 1
+    assert proxy_pop(l) == 6
+    assert proxy_pop(l) == 3
