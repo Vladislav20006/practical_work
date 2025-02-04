253c420e0d10d4849a2a5e0b48820d414cabbaf8
Vladislav20006 <sergio20002306@gmail.com>
2025-02-04 15:11:50 +0200

    Completed L11

---
+++ l11_oop/task2.py
@@ -0,0 +1,14 @@
+# Add a constructor to the class Vehicle
+class Vehicle:
+    def __init__(self, name):
+        self.name = name
+
+
+if __name__ == "__main__":
+    # Assign the needed value to the variable `c`
+    # to make the script work without errors
+    c = Vehicle("Mercedes")
+    print(c.name)
+
+    assert isinstance(c, Vehicle)
+    assert c.name == "Mercedes"
